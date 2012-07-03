69985874efbb1d03b85c03e69765dd87543e70c3
Patrick van Dissel <pvdissel@gmail.com>
2012-07-03 22:00:06 +0200

    Added IT skelet

---
+++ wallboard-datacollector-pusher/src/it/java/wallboard/tasks/FetchHudsonJobsIT.java
@@ -0,0 +1,11 @@
+package wallboard.tasks;
+
+public class FetchHudsonJobsIT {
+
+    /*
+    - Make use of httpClient to fetch url's
+    - Create HudsonJob objects from returned hudson job content
+    - Set the HudsonJobList with HudsonJob objects on the JobResult object
+     */
+
+}
