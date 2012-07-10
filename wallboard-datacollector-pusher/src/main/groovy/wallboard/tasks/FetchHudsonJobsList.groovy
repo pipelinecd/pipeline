83139f490219833c162c79eccbbc93564aa14ef4
Patrick van Dissel <pvdissel@gmail.com>
2012-07-10 20:52:16 +0200

    Added groovy compiler and cleaned dependencies

--- wallboard-datacollector-pusher/src/main/groovy/wallboard/tasks/FetchHudsonJobsList.groovy
+++ wallboard-datacollector-pusher/src/main/groovy/wallboard/tasks/FetchHudsonJobsList.groovy
@@ -5,13 +5,18 @@ import colony.Task;
 import wallboard.tasks.hudson.HudsonJob;
 import wallboard.tasks.hudson.HudsonJobList;
 
-import java.net.URI;
-
 public class FetchHudsonJobsList implements Task {
+
+
 
     @Override
     public void execute(final JobResult job) {
+
+
         final HudsonJobList jobList = new HudsonJobList();
+//        jobList.with {
+//            add()
+//        }
         jobList.add(new HudsonJob("job1", URI.create("http://localhost/job1")));
         jobList.add(new HudsonJob("job2", URI.create("http://localhost/job2")));
 
