2df263a9a2ff871b8f12536b453b7495d675eee4
Patrick van Dissel <pvdissel@gmail.com>
2012-10-02 21:02:15 +0200

    Added basic process execution

--- externalcommand/src/main/java/Command.java
+++
@@ -1,8 +0,0 @@
-public class Command {
-
-    public Command() {
-        ProcessBuilder builder = new ProcessBuilder("echo \"yeah!\"");
-//        builder.environment().
-    }
-
-}
---
+++ externalcommand/src/test/java/command/external/CommandTest.java
@@ -0,0 +1,19 @@
+package command.external;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CommandTest {
+
+    @Test
+    public void runCommand() {
+        final Command command = new Command();
+        try {
+            final int exitCode = command.run();
+            assertEquals(exitCode, 10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
