72f31ad100cae76256287847b7035612351cb8bc
Patrick van Dissel <pvdissel@gmail.com>
2012-12-11 21:28:25 +0100

    Added idea plugin to gradle and first unittest

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void name() {
+        assertTrue(true);
+    }
+}
