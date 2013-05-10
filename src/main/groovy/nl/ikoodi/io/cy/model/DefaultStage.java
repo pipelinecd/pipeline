5f9598f57b8e4a6958389faa85993689aad0f738
Patrick van Dissel <pvdissel@gmail.com>
2013-05-10 16:49:42 +0200

    #3 Pushing down the execution of external commands/processes

--- src/main/groovy/nl/ikoodi/io/cy/model/DefaultStage.java
+++ src/main/groovy/nl/ikoodi/io/cy/model/DefaultStage.java
@@ -1,12 +1,15 @@
 package nl.ikoodi.io.cy.model;
 
 import nl.ikoodi.io.cy.api.Stage;
+import org.apache.maven.shared.utils.cli.CommandLineException;
 import org.apache.maven.shared.utils.cli.CommandLineUtils;
 import org.apache.maven.shared.utils.cli.Commandline;
 import org.apache.maven.shared.utils.cli.WriterStreamConsumer;
 
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.util.Properties;
 
 public class DefaultStage implements Stage {
     private final String name;
@@ -33,14 +36,57 @@ public class DefaultStage implements Stage {
 
     @Override
     public void run(final String command) throws Exception {
-        System.out.println();
-        final Commandline cl = new Commandline(command);
-        final WriterStreamConsumer stdOut = new WriterStreamConsumer(new BufferedWriter(new OutputStreamWriter(System.out)));
-        final WriterStreamConsumer stdErr = new WriterStreamConsumer(new BufferedWriter(new OutputStreamWriter(System.err)));
-        final int exitStatus = CommandLineUtils.executeCommandLine(cl, stdOut, stdErr);
-        if (0 != exitStatus) {
-            final String msg = "Error occurred during execution of command [%s]";
-            throw new RuntimeException(String.format(msg, command));
+        final ExternalProcess process = new ExternalProcess();
+        process.setCommand(command);
+        process.run();
+    }
+
+    public class ExternalProcess {
+
+        private PrintStream out = System.out;
+        private PrintStream err = System.err;
+        private String command;
+        private Properties environmentVariables = new Properties();
+
+        public String getCommand() {
+            return command;
+        }
+
+        public void setCommand(final String command) {
+            this.command = command;
+        }
+
+        public Properties getEnvironmentVariables() {
+            return environmentVariables;
+        }
+
+        public void setEnvironmentVariables(Properties environmentVariables) {
+            this.environmentVariables = environmentVariables;
+        }
+
+        public void run() {
+            out.println();
+            final Commandline cl = new Commandline(command);
+            final int exitStatus = executeCommand(cl, createStreamingConsumer(out), createStreamingConsumer(err));
+            if (0 != exitStatus) {
+                final String msg = "Error occurred during execution of command [%s]";
+                throw new RuntimeException(String.format(msg, command));
+            }
+        }
+
+        private int executeCommand(final Commandline cl,
+                                   final WriterStreamConsumer stdOut, final WriterStreamConsumer stdErr) {
+            final int exitStatus;
+            try {
+                exitStatus = CommandLineUtils.executeCommandLine(cl, stdOut, stdErr);
+            } catch (CommandLineException e) {
+                throw new RuntimeException("Failed to execute command", e);
+            }
+            return exitStatus;
+        }
+
+        private WriterStreamConsumer createStreamingConsumer(final PrintStream stream) {
+            return new WriterStreamConsumer(new BufferedWriter(new OutputStreamWriter(stream)));
         }
     }
 
